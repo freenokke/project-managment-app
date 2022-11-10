a93bfd56b4e3ddfa22599135c6f85468c877337b
IlyaK <ilyxakotsur@gmail.com>
2022-11-10 23:33:38 +0300

    feat: add initial files

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App text-blue-500 text-5xl flex justify-center items-center h-screen">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
