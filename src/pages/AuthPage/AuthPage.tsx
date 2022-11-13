b6e802aa3571889ea5e47d9bd77536c4f9103688
IlyaK <ilyxakotsur@gmail.com>
2022-11-13 23:50:15 +0300

    feat: add custom btn to tailwind config

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { IFormInputs } from './AuthPage.types';
+import { useTranslation } from 'react-i18next';
+import i18n from '../../i18next/i18next';
 
 const schema = yup
   .object({
@@ -11,9 +14,23 @@ const schema = yup
   })
   .required();
 
-const inputsArray = ['name' as const, 'login' as const, 'password' as const];
-
 const AuthPage = () => {
+  const { t } = useTranslation();
+  const inputsArray = [
+    {
+      name: 'name' as const,
+      labelText: t('name'),
+    },
+    {
+      name: 'login' as const,
+      labelText: t('login'),
+    },
+    {
+      name: 'password' as const,
+      labelText: t('password'),
+    },
+  ];
+
   const {
     register,
     handleSubmit,
@@ -22,6 +39,10 @@ const AuthPage = () => {
 
   const onSubmit = (data: IFormInputs) => alert(JSON.stringify(data));
 
+  const changeLang = () => {
+    i18n.changeLanguage('ru');
+  };
+
   const classNames = {
     name: errors.name ? 'signInLabelError' : 'signInLabel',
     login: errors.login ? 'signInLabelError' : 'signInLabel',
@@ -29,60 +50,34 @@ const AuthPage = () => {
   };
 
   return (
-    <div className="container">
-      <div className=" shadow-md w-[500px] mx-auto flex flex-col items-center">
-        <h1 className="text-2xl mb-10">Sign in</h1>
+    <div className="container flex items-center justify-center mt-[10%]">
+      <button className="button1" onClick={changeLang}>
+        Сменить язык
+      </button>
+      <div className=" shadow-md p-5 w-[500px] mx-auto flex flex-col items-center">
+        <h1 className="text-2xl mb-10 capitalize">{t('registration')}</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
           {inputsArray.map((item) => {
             return (
-              <div className="mb-10 relative" key={item}>
-                <label htmlFor="name" className={classNames[item]}>
+              <div className="mb-10 relative" key={item.name}>
+                <label htmlFor="name" className={classNames[item.name]}>
                   <input
                     className="outline-none p-1 rounded-lg text-black"
-                    {...register(item)}
+                    {...register(item.name)}
                     type="text"
-                    name={item}
+                    name={item.name}
+                    autoComplete="off"
                   />
                   <div className=" absolute left-2 top-[-19px] p-1 bg-white uppercase text-xs ">
-                    {item}
+                    {item.labelText}
                   </div>
                 </label>
-                {errors[item] && <p className="text-red-600">{errors[item]?.message}</p>}
+                {errors[item.name] && <p className="text-red-600">{errors[item.name]?.message}</p>}
               </div>
             );
           })}
-          {/* <div className="mb-10 relative">
-            <label htmlFor="name" className={nameLabelClass}>
-              <input
-                className="outline-none p-1 rounded-lg text-black"
-                {...register('name')}
-                type="text"
-                name="name"
-              />
-              <div className=" absolute left-2 top-[-19px] p-1 bg-white uppercase text-xs ">
-                Name
-              </div>
-            </label>
-            {errors.name && <p className="text-red-600">{errors.name?.message}</p>}
-          </div>
-
-          <div className="mb-10 relative">
-            <label htmlFor="login" className={loginLabelClass}>
-              <input
-                className="outline-none p-1 rounded-lg text-black"
-                {...register('login')}
-                type="text"
-                name="login"
-              />
-              <div className=" absolute left-2 top-[-19px] p-1 bg-white uppercase text-xs ">
-                Login
-              </div>
-            </label>
-            {errors.login && <p className="text-red-600">{errors.login?.message}</p>}
-          </div> */}
-
-          <button type="submit" className="mt-10">
-            Accept
+          <button type="submit" className="button1 capitalize">
+            {t('signUp')}
           </button>
         </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -38,9 +38,10 @@ module.exports = {
       addComponents({
         '.signInLabel': {
           padding: '10px',
-          border: '2px solid #CBD5E1 ',
+          border: '2px solid #c1d3f4',
           color: '#CBD5E1',
           borderRadius: '8px',
+          transition: 'all 0.3s',
           '&:focus-within': {
             borderColor: '#2563EB',
             color: '#2563EB',
@@ -51,6 +52,39 @@ module.exports = {
           border: '2px solid rgb(220, 38, 38)',
           color: 'rgb(220, 38, 38)',
           borderRadius: '8px',
+        },
+        '.button1': {
+          display: 'inline-block',
+          boxSizing: 'border-box',
+          padding: '0 20px',
+          margin: '0',
+          outline: 'none',
+          border: 'none',
+          borderRadius: '8px',
+          height: '40px',
+          lineHeight: '40px',
+          fontSize: '17px',
+          fontWeight: '600',
+          textDecoration: 'none',
+          color: '#385898',
+          backgroundColor: '#e7f3ff',
+          cursor: 'pointer',
+          userSelect: 'none',
+          appearance: ' none',
+          touchAction: 'manipulation',
+          '&:focus-visible': {
+            boxShadow: '0 0 0 2px #666',
+          },
+          '&:hover': {
+            backgroundColor: '#DBE7F2',
+          },
+          '&:active': {
+            transform: 'scale(0.96)',
+          },
+          '&:disabled': {
+            pointerEvents: 'none',
+            opacity: '0.65',
+          },
         },
       });
     }),
