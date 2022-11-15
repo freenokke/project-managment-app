00f21880add1d4590c87ddbda87283ac5d0c6768
IlyaK <ilyxakotsur@gmail.com>
2022-11-15 13:20:06 +0300

    feat: add links under form to redirect signp/signin pages

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { signIn } from '../../redux/features/authSlice';
 import { AppDispatch, RootState } from '../../redux/store';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import SignInput from '../../components/SignInput/SignInput';
 import { Button } from '@material-tailwind/react';
 
@@ -45,14 +45,23 @@ const SignInPage = () => {
     <div className="container flex items-center justify-center mt-[10%]">
       <div className=" shadow-md p-5 w-[500px] mx-auto flex flex-col items-center">
         <h1 className="text-2xl mb-10 capitalize">{t('signIn')}</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-11">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-11 mb-3">
           <SignInput name="login" error={errors?.login} register={register} />
           <SignInput name="password" error={errors?.password} register={register} password />
 
           <Button type="submit" disabled={isLoading}>
-            {isLoading ? t('signUp.loading') : t('signUp')}
+            {isLoading ? t('signUp.loading') : t('signIn')}
           </Button>
         </form>
+
+        {error && <div className="text-red-600 ">{error?.message}</div>}
+
+        <Link
+          to="/signup"
+          className="mt-3 text-gray-400 transition-colors cursor-pointer hover:text-blue-600 "
+        >
+          {t('signIn.redirectToSignUp')}
+        </Link>
       </div>
     </div>
   );
