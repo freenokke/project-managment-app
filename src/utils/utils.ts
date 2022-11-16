60db59d60bb2cf9b415afa8d5633c94f9c40b32a
IlyaK <ilyxakotsur@gmail.com>
2022-11-16 19:59:59 +0300

    feat: add token and userId to localStorage

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+export function generateRandomIntegerInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function parseJwt(token: string) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
