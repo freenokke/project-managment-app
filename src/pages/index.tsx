c760eff8e976b5479aa10216836e9234f512223f
Olga Pochenkova <aminkkaby@gmail.com>
2022-11-17 13:03:54 +0300

    feat: add board page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import MainPage from './MainPage/MainPage';
 import WelcomePage from './WelcomePage/WelcomePage';
+import BoardPage from './BoardPage/BoardPage';
 import SignUpPage from './SignUpPage/SignUpPage';
 import SignInPage from './SignInPage/SignInPage';
 
-export { MainPage, WelcomePage, SignUpPage, SignInPage };
+export { MainPage, WelcomePage, BoardPage, SignUpPage, SignInPage };
