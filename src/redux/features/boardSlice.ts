e361bd95269756bc70f43b6fb4066fe1a1694b06
Olga Pochenkova <aminkkaby@gmail.com>
2022-11-23 22:06:48 +0300

    feat: create board state in redux

---
+++ src/redux/features/boardSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface IBoardState {
+  boardId: string;
+  columnId: string;
+  taskId: string;
+}
+
+const initialState: IBoardState = {
+  boardId: '',
+  columnId: '',
+  taskId: '',
+};
+
+const boardSlice = createSlice({
+  name: 'board',
+  initialState,
+  reducers: {
+    openBoard(state, action: PayloadAction<string>) {
+      state.boardId = action.payload;
+    },
+  },
+});
+
+export const { openBoard } = boardSlice.actions;
+export default boardSlice;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modalSlice from './features/modalSlice';
+import boardSlice from './features/boardSlice';
 import { boardsApi } from './api/boardsApi';
 import auth from './features/authSlice';
 
 export const store = configureStore({
   reducer: {
     [modalSlice.name]: modalSlice.reducer,
+    [boardSlice.name]: boardSlice.reducer,
     [boardsApi.reducerPath]: boardsApi.reducer,
     auth,
   },
