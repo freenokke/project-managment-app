61b224bd21044abf992ad02826bc0c09a3d150d3
freenokke <kosmatkov94@gmail.com>
2022-11-24 23:08:12 +0300

    feat: implement testing board page

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,5 +1,16 @@
+import { InnerColumn } from '../../components';
+import Modal from '../../components/Modal/Modal';
+
 const BoardPage = () => {
-  return <h1>Board Page</h1>;
+  return (
+    <div onDrop={() => console.log('drop')} className="container flex-grow">
+      <h3>IlyyxaColumnWrapper</h3>
+      <div className="h-[500px] w-[200px]">
+        <InnerColumn boardId="63790c00255b5e6beda3406b" columnId="637ca84f255b5e6beda34194" />
+      </div>
+      <Modal />
+    </div>
+  );
 };
 
 export default BoardPage;
