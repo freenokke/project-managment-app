0183ef1f9db869b4a53020c02347ba2b9119ee69
freenokke <kosmatkov94@gmail.com>
2022-11-27 19:41:17 +0300

    feat: improve drag&drop functionality to transfer tasks between columns

--- src/hooks/useDraggable.tsx
+++ src/hooks/useDraggable.tsx
@@ -5,43 +5,59 @@ import { useAppDispatch, useAppSelector } from './redux.hooks';
 import { setCurrentDraggable } from '../redux/features/dragSlice';
 
 export const useDraggable = () => {
-  const [updateTasksSetCall, {}] = useUpdateSetOfTasksMutation();
+  const [updateTasksSetCall, { isLoading: isUpdate }] = useUpdateSetOfTasksMutation();
   const dispatch = useAppDispatch();
   const currentDraggable = useAppSelector((state) => state.drag.currentDraggable);
 
   const updateTasksSet = useCallback(
-    async (data: { _id: string; order: number; columnId: string }[]) => {
+    async (data: Pick<ITaskData, '_id' | 'order' | 'columnId'>[]) => {
       await updateTasksSetCall(data).unwrap();
     },
     [updateTasksSetCall]
   );
 
   const dragDropHandler = useCallback(
-    (e: React.DragEvent<HTMLDivElement>, data: ITaskData) => {
+    (e: React.DragEvent, data: ITaskData | Pick<ITaskData, 'columnId'>) => {
       e.stopPropagation();
       e.preventDefault();
-      if (currentDraggable && data.order !== currentDraggable?.order) {
-        const {
-          _id: draggedElemId,
-          order: draggedElemOrder,
-          columnId: draggedElemCol,
-        } = currentDraggable;
-        const { _id: droppedElemId, order: droppedElemOrder, columnId: droppedElemCol } = data;
-
-        const body = [
-          {
+      //если order присутствует в объекте, значит дроп происходит на таске, а не на колонке.
+      if ('order' in data) {
+        if (
+          currentDraggable &&
+          (data.order !== currentDraggable?.order || data.columnId !== currentDraggable?.columnId)
+        ) {
+          const {
             _id: draggedElemId,
-            order: droppedElemOrder,
-            columnId: droppedElemCol,
-          },
-          {
-            _id: droppedElemId,
             order: draggedElemOrder,
             columnId: draggedElemCol,
-          },
-        ];
+          } = currentDraggable;
+          const { _id: droppedElemId, order: droppedElemOrder, columnId: droppedElemCol } = data;
 
-        updateTasksSet(body);
+          const requestBody = [
+            {
+              _id: draggedElemId,
+              order: droppedElemOrder,
+              columnId: droppedElemCol,
+            },
+            {
+              _id: droppedElemId,
+              order: draggedElemOrder,
+              columnId: draggedElemCol,
+            },
+          ];
+          updateTasksSet(requestBody);
+        }
+      } else {
+        if (currentDraggable && data.columnId !== currentDraggable.columnId) {
+          const requestBody = [
+            {
+              _id: currentDraggable._id,
+              order: currentDraggable.order,
+              columnId: data.columnId,
+            },
+          ];
+          updateTasksSet(requestBody);
+        }
       }
       (e.target as HTMLDivElement).classList.remove('shadow', 'shadow-blue-400');
     },
@@ -49,14 +65,14 @@ export const useDraggable = () => {
   );
 
   const dragStartHandler = useCallback(
-    (e: React.DragEvent<HTMLDivElement>, data: ITaskData) => {
+    (e: React.DragEvent, data: ITaskData) => {
       e.stopPropagation();
       dispatch(setCurrentDraggable(data));
     },
     [dispatch]
   );
 
-  const dragOverHandler = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+  const dragOverHandler = useCallback((e: React.DragEvent) => {
     e.stopPropagation();
     e.preventDefault();
     (e.target as HTMLDivElement).classList.add('shadow', 'shadow-blue-400');
@@ -68,7 +84,7 @@ export const useDraggable = () => {
   }, []);
 
   const dragEndHandler = useCallback(
-    (e: React.DragEvent<HTMLDivElement>) => {
+    (e: React.DragEvent) => {
       e.stopPropagation();
       (e.target as HTMLDivElement).classList.remove('shadow', 'shadow-blue-400');
       dispatch(setCurrentDraggable(null));
@@ -83,5 +99,6 @@ export const useDraggable = () => {
     dragOverHandler,
     dragStartHandler,
     updateTasksSet,
+    isUpdate,
   };
 };
