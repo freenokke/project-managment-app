45836ac01d4d175bbd7420155949b29cd1b4043c
freenokke <kosmatkov94@gmail.com>
2022-12-06 02:49:53 +0300

    fix: fix drag&drop bug

--- src/components/Modal/useTaskModal.tsx
+++ src/components/Modal/useTaskModal.tsx
@@ -1,15 +1,18 @@
 import {
   ITaskCreate,
   ITaskUpdate,
+  reorder,
   useCreateTaskMutation,
   useDeleteTaskMutation,
   useEditTaskMutation,
+  useUpdateSetOfTasksMutation,
 } from '../../redux/api/tasksApi';
-import { useAppDispatch } from '../../hooks/redux.hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux.hooks';
 import { ModalData } from './Modal.types';
 import 'react-toastify/dist/ReactToastify.css';
 import { useEffect } from 'react';
 import { setIsLoadingTask } from '../../redux/features/boardInfoSlice';
+import { setLocalTasks } from '../../redux/features/localDataSlice';
 
 export interface Updates {
   title: string;
@@ -26,6 +29,8 @@ export const useTaskModal = () => {
     useEditTaskMutation();
   const [deleteTaskCall, { isLoading: isDeletingTask, isError: deletingTaskError }] =
     useDeleteTaskMutation();
+  const [updateTasksSetCall, {}] = useUpdateSetOfTasksMutation();
+  const displayedData = useAppSelector((state) => state.localData);
 
   useEffect(() => {
     dispatch(setIsLoadingTask(isEditingTask || isDeletingTask));
@@ -36,7 +41,24 @@ export const useTaskModal = () => {
   };
 
   const deleteTask = async (data: ModalData) => {
-    await deleteTaskCall(data);
+    await deleteTaskCall(data)
+      .unwrap()
+      .then((res) => {
+        const rebuiltLocalTasks = displayedData
+          .find((item) => item.column === res.columnId)
+          ?.tasks?.filter((task) => task._id !== res._id);
+        dispatch(setLocalTasks({ tasks: rebuiltLocalTasks ?? [], columnId: res.columnId }));
+        if (rebuiltLocalTasks && rebuiltLocalTasks.length > 1) {
+          const requestBody = [
+            ...rebuiltLocalTasks?.map(reorder).map((task) => ({
+              _id: task._id,
+              order: task.order,
+              columnId: task.columnId,
+            })),
+          ];
+          updateTasksSetCall({ body: requestBody });
+        }
+      });
   };
 
   const editTask = async (data: ITaskUpdate) => {
